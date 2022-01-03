e54d3d3bc09165f6559962fa6be739d9aea124c4
Adriel Arruda <adriel.marcos.arruda@gmail.com>
2022-01-03 11:53:55 -0300

    Add project files.

---
+++ Heranca/Entities/BusinessAccount.cs
@@ -0,0 +1,22 @@
+﻿namespace Inheritance.Entities
+{
+    class BusinessAccount : Account
+    {
+        public double LoanLimite { get; set; }
+    
+        public BusinessAccount(int number, string holder, double balance, double loanLimite) : base(number, holder, balance)
+        {
+            LoanLimite = loanLimite;
+        }
+
+        public void Loan(double amount)
+        {
+            if (amount <= LoanLimite)
+            {
+                Balance += amount;
+            }
+        }
+    
+    
+    }
+}
